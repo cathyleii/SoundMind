d3a5730b4cfc535d275b769208fa7a9fa0a6a2bf
cathylei <zlei04@students.cs.ubc.ca>
2025-06-05 23:53:23 -0700

    fixed bug in /submit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request, session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
-import requests
 import json
 import spotify
 import os
@@ -15,6 +14,19 @@ load_dotenv()
 JOY_PLAYLIST1 = "spotify:playlist:7GhawGpb43Ctkq3PRP1fOL"
 JOY_PLAYLIST2 = "spotify:playlist:4Fh0313D3PitYzICKHhZ7r"
 
+SAD_PLAYLIST1 = "spotify:playlist:1XE7rQIGl1NFtWEAfwn4b9"
+
+SAD_FEAR_COMFORT_PLAYLIST = "spotify:playlist:2cLa9xr9SArrzy0wXnW8m2"
+
+FEAR_PLAYLIST1 = "spotify:playlist:12M8uwtzZqKHKSbBFbhGFy"
+FEAR_PLAYLIST_INSTRUMENTALS = "spotify:playlist:7LI3zw8HLkjKo5YpvA26KG"
+
+ANGER_PLAYLIST1 = "spotify:playlist:10egOYgYVWoviGAfdpiRty"
+ANGER_PLAYLIST_RAP = "spotify:playlist:4WEn4bQ84SdvLIkwWqa1H8"
+
+NEUTRAL_TOP_50 = "spotify:playlist:37i9dQZEVXbMDoHDwVN2tF"
+NEUTRAL_TODAYS_HITS = "spotify:playlist:37i9dQZF1DXcBWIGoYBM5M"
+
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 
@@ -24,11 +36,12 @@ app.secret_key = os.getenv("FLASK_SECRET_KEY")
 # which URL is associated function 
 @app.route('/')       
 def index():
-    return render_template("index.html")
+    unfilled_fields = session.pop("unfilled_fields", False)
+    return render_template("index.html", unfilled_fields=unfilled_fields)
 
 @app.route('/analysis-complete')
 def analysis_complete():
-    analysis = session.get("analysis", "error")
+    analysis = session.get("analysis", "Error: Something went wrong, please try again.")
 
     joy_tracks1 = spotify.retrieve_playlist_tracks(JOY_PLAYLIST1)
     joy_tracks2 = spotify.retrieve_playlist_tracks(JOY_PLAYLIST2)
@@ -47,14 +60,23 @@ def analysis_complete():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    text = request.form.getlist("text")
-    input_string = ''
-
-    for ans in text:
-        input_string += f"{ans} "
-    print(input_string)
-    session["analysis"] = analyze_text(input_string)
-    return redirect("/analysis-complete")
+    try:
+        q1_text = request.form["q1_text"]
+        q2_text = request.form["q2_text"]
+        q3_text = request.form.get("q3_text")
+        mood = request.form["mood"]
+    except KeyError:
+        session["unfilled_fields"] = True
+        return redirect("/")
+    else:
+        if not q1_text or not q2_text:
+            session["unfilled_fields"] = True
+            return redirect("/")
+        
+        input_string = f"I feel {mood} because {q1_text}. {q2_text} {q3_text}"
+        print(input_string)
+        session["analysis"] = analyze_text(input_string)
+        return redirect("/analysis-complete")
     
 
 
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,4 +3,9 @@ from transformers import pipeline
 classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 
 def analyze_text(text):
-    return classifier(text)
+    results = classifier(text)
+    results = results[0]  
+    return results
+
+def get_top_emotion(analysis):
+    return analysis[0]['label']
